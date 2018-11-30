bc625699f91342c1b71111a2ff813f383f22d110
Steffen Maechtel <info@steffen-maechtel.de>
2018-11-30 17:58:10 +0100

    Throw exception instead of show debug message

--- Classes/Xclass/ExceptionHandlingDatabaseConnection.php
+++ Classes/Xclass/ExceptionHandlingDatabaseConnection.php
@@ -1,14 +1,21 @@
 <?php
 namespace SteffenMaechtel\ExceptionHandlingForTypo3Db\Xclass;
 
+use Exception;
+use TYPO3\CMS\Core\Database\DatabaseConnection;
+
 /**
  * @author Steffen Maechtel <info@steffen-maechtel.de>
  */
-
-use TYPO3\CMS\Core\Database\DatabaseConnection;
-
 class ExceptionHandlingDatabaseConnection extends DatabaseConnection
 {
 
-    // @todo
+    public function debug($func, $query = '')
+    {
+        $this->logDeprecation();
+        $error = $this->sql_error();
+        if ($error) {
+            throw new Exception($error, 1543596854);
+        }
+    }
 }
