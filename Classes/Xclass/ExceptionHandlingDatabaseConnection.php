94547af8e786d4c0769232295aa663cae14eb8a0
Steffen Maechtel <info@steffen-maechtel.de>
2018-11-30 17:26:22 +0100

    ADD dummy class

---
+++ Classes/Xclass/ExceptionHandlingDatabaseConnection.php
@@ -0,0 +1,14 @@
+<?php
+namespace SteffenMaechtel\ExceptionHandlingForTypo3Db\Xclass;
+
+/**
+ * @author Steffen Maechtel <info@steffen-maechtel.de>
+ */
+
+use TYPO3\CMS\Core\Database\DatabaseConnection;
+
+class ExceptionHandlingDatabaseConnection extends DatabaseConnection
+{
+
+    // @todo
+}
